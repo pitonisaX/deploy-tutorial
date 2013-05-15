a7e26cb48dddb988220b4a391ac534d5a343810f
pitonisax <karreyes@gmail.com>
2013-05-14 21:19:46 -0700

    added tab

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,10 @@ env.user = ['pitonisax']
 env.hosts = ['pycourse.com']
 
 def deploy():
-    code_dir + 'home/pitonisax/blog'
+    code_dir = 'home/pitonisax/blog'
     with cd(code_dir):
-    run('git pull')
-    run('bin/pelican -s mysite.py')
+        run('git pull')
+        run('bin/pelican -s mysite.py')
 
 def check_distro():
     run('cat /etc/*release*')
